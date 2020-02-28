3bddcd123b8d112117847008e8cfaba3952223f8
Tomi Poveda <tpovedatd@gmail.com>
2020-02-28 03:12:13 +0100

    Remove some unnecessary modules

--- tpDcc/libs/qt/managers/preferences.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Module that contains implementation for preferences manager
-"""
-
-from __future__ import print_function, division, absolute_import
