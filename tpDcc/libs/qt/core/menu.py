04a73c965e6d029f299957dc21e8ac034a01fb6b
Tomas Poveda <tpovedatd@gmail.com>
2020-04-12 03:29:49 +0200

    Added base menu widget

--- tpDcc/libs/qt/core/menu.py
+++ tpDcc/libs/qt/core/menu.py
@@ -10,7 +10,155 @@ from __future__ import print_function, division, absolute_import
 from Qt.QtCore import *
 from Qt.QtWidgets import *
 
-from tpDcc.libs.qt.core import qtutils
+from tpDcc.libs.python import python
+from tpDcc.libs.qt.core import mixin, qtutils, formatters
+
+
+@mixin.theme_mixin
+@mixin.property_mixin
+class BaseMenu(QMenu, object):
+
+    valueChanged = Signal(list)
+
+    def __init__(self, exclusive=True, cascader=False, title='', parent=None):
+        super(BaseMenu, self).__init__(title=title, parent=parent)
+
+        self._load_data_fn = None
+
+        self._action_group = QActionGroup(self)
+        self._action_group.setExclusive(exclusive)
+        self._action_group.triggered.connect(self._on_action_triggered)
+
+        self.setProperty('cascader', cascader)
+        self.setCursor(Qt.PointingHandCursor)
+
+        self.set_value('')
+        self.set_data([])
+        self.set_separator('/')
+
+    # =================================================================================================================
+    # BASE
+    # =================================================================================================================
+
+    def set_separator(self, separator_character):
+        """
+        Sets menu separator character
+        :param separator_character: str
+        """
+        self.setProperty('separator', separator_character)
+
+    def set_value(self, data):
+        """
+        Sets menu value
+        :param data:  str, int or float
+        """
+
+        assert isinstance(data, (list, str, unicode, int, float))
+        if self.property('cascader') and isinstance(data, (str, unicode)):
+            data = data.split(self.property('separator'))
+        self.setProperty('value', data)
+
+    def set_data(self, option_list):
+        """
+        Sets menu data
+        :param option_list: list
+        """
+
+        assert isinstance(option_list, list)
+        if option_list:
+            if all(isinstance(i, (str, unicode)) for i in option_list):
+                option_list = python.from_list_to_nested_dict(option_list, separator=self.property('separator'))
+            if all(isinstance(i, (int, float)) for i in option_list):
+                option_list = [{'value': i, 'label': str(i)} for i in option_list]
+        self.setProperty('data', option_list)
+
+    def set_loader(self, fn):
+        """
+        Sets menu loader
+        :param fn: function
+        """
+
+        self._load_data_fn = fn
+
+    def set_load_callback(self, fn):
+        """
+        Sets menu load callback
+        :param fn: function
+        """
+
+        assert callable(fn)
+        self._load_data_fn = fn
+        self.aboutToShow.connect(self._on_fetch_data)
+
+    # =================================================================================================================
+    # PROPERTY MIXIN SETTERS
+    # =================================================================================================================
+
+    def _set_value(self, value):
+        data_list = value if isinstance(value, list) else [value]
+        flag = False
+        for act in self._action_group.actions():
+            checked = act.property('value') in data_list
+            if act.isChecked() != checked:
+                act.setChecked(checked)
+                flag = True
+
+        if flag:
+            self.valueChanged.emit(value)
+
+    def _set_data(self, option_list):
+        self.clear()
+        for act in self._action_group.actions():
+            self._action_group.removeAction(act)
+        for data_dict in option_list:
+            self._add_menu(self, data_dict)
+
+    # =================================================================================================================
+    # INTERNAL
+    # =================================================================================================================
+
+    def _add_menu(self, parent_menu, data_dict):
+        if 'children' in data_dict:
+            menu = BaseMenu(title=data_dict.get('label'), parent=self)
+            menu.setProperty('value', data_dict.get('value'))
+            parent_menu.addMenu(menu)
+            if parent_menu is not self:
+                menu.setProperty('parent_menu', parent_menu)
+            for i in data_dict.get('children'):
+                self._add_menu(menu, i)
+        else:
+            action = self._action_group.addAction(formatters.display_formatter(data_dict.get('label')))
+            action.setProperty('value', data_dict.get('value'))
+            action.setCheckable(True)
+            action.setProperty('parent_menu', parent_menu)
+            parent_menu.addAction(action)
+
+    def _get_parent(self, result, obj):
+        if obj.property('parent_menu'):
+            parent_menu = obj.property('parent_menu')
+            result.insert(0, parent_menu.title())
+            self._get_parent(result, parent_menu)
+
+    # =================================================================================================================
+    # CALLBACKS
+    # =================================================================================================================
+
+    def _on_action_triggered(self, action):
+        current_data = action.property('value')
+        if self.property('cascader'):
+            selected_data = [current_data]
+            self._get_parent(selected_data, action)
+        else:
+            if self._action_group.isExclusive():
+                selected_data = current_data
+            else:
+                selected_data = [act.property('value') for act in self._action_group.actions() if act.isChecked()]
+        self.set_value(selected_data)
+        self.valueChanged.emit(selected_data)
+
+    def _on_fetch_data(self):
+        data_list = self._load_data_fn()
+        self.set_data(data_list)
 
 
 class Menu(QMenu, object):
