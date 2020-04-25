93b2f2bd7befe38f7f869d7947f9effd260ba38c
Tomas Poveda <tpovedatd@gmail.com>
2020-04-25 05:03:50 +0200

    Added new base toast widget

--- tpDcc/libs/qt/widgets/toast.py
+++ tpDcc/libs/qt/widgets/toast.py
@@ -11,7 +11,154 @@ from Qt.QtCore import *
 from Qt.QtWidgets import *
 from Qt.QtGui import *
 
-from tpDcc.libs.qt.core import animation
+import tpDcc as tp
+from tpDcc.libs.qt.core import base, mixin, animation, theme
+from tpDcc.libs.qt.widgets import label, avatar, loading
+
+
+@mixin.theme_mixin
+class BaseToast(base.BaseWidget, object):
+
+    class ToastTypes(object):
+        INFO = 'info'
+        SUCCESS = 'success'
+        WARNING = 'warning'
+        ERROR = 'error'
+        LOADING = 'loading'
+
+    DEFAULT_CONFIG = {'duration': 2}
+
+    toastClosed = Signal()
+
+    def __init__(self, text, duration=None, toast_type=None, parent=None):
+        self._text = text
+        self._duration = duration
+        self._toast_type = toast_type
+        self._parent = parent
+        super(BaseToast, self).__init__(parent=parent)
+
+    def get_main_layout(self):
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(0, 0, 0, 0)
+
+        return main_layout
+
+    def ui(self):
+        super(BaseToast, self).ui()
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Dialog | Qt.WA_TranslucentBackground | Qt.WA_DeleteOnClose)
+        self.setAttribute(Qt.WA_StyledBackground)
+        self.setFixedSize(QSize(120, 120))
+
+        icon_layout = QHBoxLayout()
+        icon_layout.addStretch()
+
+        widget_theme = self.theme()
+
+        if self._toast_type == self.ToastTypes.LOADING:
+            icon_layout.addWidget(loading.CircleLoading(size=widget_theme.huge, color=widget_theme.text_color_inverse))
+        else:
+            icon_label = avatar.Avatar()
+            icon_label.theme_size = 60
+            icon_label.image = tp.ResourcesMgr().pixmap(
+                self._toast_type or self.ToastTypes.INFO, color=widget_theme.text_color_inverse)
+            icon_layout.addWidget(icon_label)
+        icon_layout.addStretch()
+
+        content_label = label.BaseLabel()
+        content_label.setText(self._text or '')
+        content_label.setAlignment(Qt.AlignCenter)
+
+        self.main_layout.addStretch()
+        self.main_layout.addLayout(icon_layout)
+        self.main_layout.addSpacing(10)
+        self.main_layout.addWidget(content_label)
+        self.main_layout.addStretch()
+
+        close_timer = QTimer(self)
+        close_timer.setSingleShot(True)
+        close_timer.timeout.connect(self.close)
+        close_timer.timeout.connect(self.toastClosed.emit)
+        close_timer.setInterval((self._duration or self.DEFAULT_CONFIG.get('duration', 2)) * 1000)
+
+        anim_timer = QTimer(self)
+        anim_timer.timeout.connect(self._fade_out)
+        anim_timer.setInterval((self._duration or self.DEFAULT_CONFIG.get('duration', 2)) * 1000 - 300)
+
+        self._opacity_anim = QPropertyAnimation()
+        self._opacity_anim.setTargetObject(self)
+        self._opacity_anim.setDuration(300)
+        self._opacity_anim.setEasingCurve(QEasingCurve.OutCubic)
+        self._opacity_anim.setPropertyName('windowOpacity')
+        self._opacity_anim.setStartValue(0.0)
+        self._opacity_anim.setEndValue(0.9)
+
+        close_timer.start()
+        anim_timer.start()
+
+        self._get_center_position(self._parent)
+        self._fade_in()
+
+    @classmethod
+    def info(cls, text, parent, duration=None):
+        inst = cls(text, duration=duration, toast_type=cls.ToastTypes.INFO, parent=parent)
+        inst.show()
+
+        return inst
+
+    @classmethod
+    def success(cls, text, parent, duration=None):
+        inst = cls(text, duration=duration, toast_type=cls.ToastTypes.SUCCESS, parent=parent)
+        inst.show()
+
+        return inst
+
+    @classmethod
+    def warning(cls, text, parent, duration=None):
+        inst = cls(text, duration=duration, toast_type=cls.ToastTypes.WARNING, parent=parent)
+        inst.show()
+
+        return inst
+
+    @classmethod
+    def error(cls, text, parent, duration=None):
+        inst = cls(text, duration=duration, toast_type=cls.ToastTypes.ERROR, parent=parent)
+        inst.show()
+
+        return inst
+
+    @classmethod
+    def loading(cls, text, parent, duration=None):
+        inst = cls(text, duration=duration, toast_type=cls.ToastTypes.LOADING, parent=parent)
+        inst.show()
+
+        return inst
+
+    @classmethod
+    def config(cls, duration):
+        if duration is not None:
+            cls.DEFAULT_CONFIG['duration'] = duration
+
+    def _fade_out(self):
+        self._opacity_anim.setDirection(QAbstractAnimation.Backward)
+        self._opacity_anim.start()
+
+    def _fade_in(self):
+        self._opacity_anim.start()
+
+    def _get_center_position(self, parent):
+        parent_parent = parent.parent()
+        dcc_win = tp.Dcc.get_main_window()
+        dcc_window = parent_parent == dcc_win or parent_parent.objectName() == dcc_win.objectName()
+        parent_geo = parent.geometry()
+        pos = parent_geo.topLeft() if dcc_window else parent.mapToGlobal(parent_geo.topLeft())
+        offset = 0
+        for child in parent.children():
+            if isinstance(child, BaseToast) and child.isVisible():
+                offset = max(offset, child.y())
+        target_x = pos.x() + parent_geo.width() / 2 - self.width() / 2
+        target_y = pos.y() + parent_geo.height() / 2 - self.height() / 2
+        self.setProperty('pos', QPoint(target_x, target_y))
 
 
 class ToastWidget(QLabel, object):
